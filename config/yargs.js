1338c94ccb7b7e79cd8bb30aa1db078d016be43e
Martin Barria Joost <martinjoostw@gmail.com>
2020-06-09 18:27:28 -0600

    yargs optimization

--- config/yargs.js
+++ config/yargs.js
@@ -1,37 +1,31 @@
 
+
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de tarea'
+}
+
+const completado = {
+    demand: true,
+    alias: 'c',
+    desc: 'Descripcion de tarea',
+    default: true
+}
+
 const argv = require('yargs')
 .command('crear', 'Creando un elemento todo', {
-    descripcion: {
-        demand: true,
-        alias: 'd',
-        desc: 'Descripcion de tarea'
-    }
+    descripcion
 })
 .command('delete', 'Eliminar Todo', {
-    descripcion: {
-        demand: true,
-        alias: 'd',
-        desc: 'Descripcion de tarea'
-    }
+    descripcion
 })
 .command('update', 'Actualiza un elemento todo', {
-    descripcion: {
-        demand: true,
-        alias: 'd',
-        desc: 'Descripcion de tarea'
-    },
-    completado: {
-        demand: true,
-        alias: 'c',
-        desc: 'Descripcion de tarea',
-        default: true
-    }
+    descripcion,
+    completado
 })
 .help()
 .argv
-
-
-
 
 
 module.exports = {
